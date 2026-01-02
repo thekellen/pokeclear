8cca0af6be881bf699219f2e93466969d35830d9
Kellen Sullivan <iam@thekellen.com>
2026-01-01 18:02:16 -0600

    set clear title

--- engine/movie/title.asm
+++ engine/movie/title.asm
@@ -406,10 +406,10 @@ PrintGameVersionOnTitleScreen:
 ; these point to special tiles specifically loaded for that purpose and are not usual text
 VersionOnTitleScreenText:
 IF DEF(_RED)
-	db $60,$61,$7F,$65,$66,$67,$68,$69,"@" ; "Red Version"
+	db $61,$62,$63,$64,$65,$66,$67,$68,"@" ; "Red Version"
 ENDC
 IF DEF(_BLUE)
-	db $7F,$7F,$7F,$7F,$65,$66,$67,$68,$69,"@" ; POKECLEAR MOD: "    Version" (spaces instead of "Blue")
+	db $61,$62,$63,$64,$65,$66,$67,$68,"@" ; "Clear Version"
 ENDC
 
 DebugNewGamePlayerName:
